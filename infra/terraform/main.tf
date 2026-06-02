272a6deb760ec55b5aa8241aa1766344fe900b95
Pa04rth <parth.22440@knit.ac.in>
2026-06-02 17:46:21 +0530

    chore: terraform fmt

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -146,9 +146,9 @@ module "eks" {
 
   # Cluster addons
   cluster_addons = {
-    coredns            = { most_recent = true }
-    kube-proxy         = { most_recent = true }
-    vpc-cni            = { most_recent = true }
+    coredns    = { most_recent = true }
+    kube-proxy = { most_recent = true }
+    vpc-cni    = { most_recent = true }
     # aws-ebs-csi-driver = { most_recent = true }
   }
 
